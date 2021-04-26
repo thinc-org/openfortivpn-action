1bc789b5e2d3d0f53cfc0419036ac5d53d660631
Suphon Thanakornpakapong <paphonb@gmail.com>
2021-04-26 14:07:36 +0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { getInput, setFailed } from '@actions/core'
+import { spawn } from 'child_process'
+
+try {
+  const username = getInput('username')
+  const password = getInput('password')
+  const hostname = getInput('host')
+  const trustedCert = getInput('trustedCert')
+  const openfortivpn = spawn(
+    'sudo',
+    ['openfortivpn', hostname, `--username=${username}`, `--trusted-cert=${trustedCert}`],
+    {
+      detached: true,
+    }
+  )
+  openfortivpn.stdin.write(`${password}\n`)
+  openfortivpn.stdout.on('data', (data) => {
+    const line = data.toString() as string
+    console.log(line)
+    if (line.includes('Tunnel is up and running')) {
+      process.exit(0)
+    } else if (line.includes('Could not authenticate to gateway')) {
+      setFailed('Failed to login')
+      process.exit(1)
+    }
+  })
+  openfortivpn.on('close', () => {
+    setFailed('Client unexpectedly closed')
+    process.exit(1)
+  })
+} catch (error) {}
